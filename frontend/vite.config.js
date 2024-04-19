11629d0249cfc88e4d011f469f95d98afac6e4ad
unknown <kyriesanchez638@hotmail.com>
2024-04-19 11:56:16 -0500

    Remove .env from tracking

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,13 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       '/stripe': 'http://localhost:5000'
-//     }
-//   }
-// })
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/stripe': 'http://localhost:5000'
+    }
+  }
+})
 
